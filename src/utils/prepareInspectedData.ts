8f93567dd1c20ad49c49983e09e1f6ae332b5e4c
chaitanyajoshi199-maker <chaitanyajoshi199@gmail.com>
2026-08-29 13:58:33 +0530

    Add only project.ipynb for week 4

--- src/utils/prepareInspectedData.ts
+++
@@ -1,9 +0,0 @@
-// import type { InspectedDataTy } from "../types";
-
-// export const prepareInspectedData = (prop:InspectedDataTy[]) => {
-
-//     const InspectedData:=[]
-//     prop.map((data)=>(
-
-//     ))
-// }
